c4c19067b13a5d89eca8e05a903cf6a0a7b416e8
Sade <berrett@protonmail.com>
2022-02-16 18:09:06 +0200

    cli.py reads any file and displays its contents

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 # Archibald, a command line tool for inspecting archimate files
 
+# create a cli that takes an archimate file as an argument
+
 
 def main():
     pass
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
