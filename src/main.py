670a2b2b067e7d28cf89cdc69c9e62949d3d46ea
Sade <berrett@protonmail.com>
2022-02-16 18:14:10 +0200

    main.py evokes functionality from cli.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 # Archibald, a command line tool for inspecting archimate files
 
 # create a cli that takes an archimate file as an argument
+import cli
 
 
 def main():
-    pass
+    cli.input_file()
 
 
 if __name__ == "__main__":
