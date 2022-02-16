188e8d2e7d9d66db38340fe79df64456b48e30ce
Sade <berrett@protonmail.com>
2022-02-16 21:28:06 +0200

    basic check to confirm it's an archimate model

--- src/cli.py
+++ src/cli.py
@@ -10,9 +10,19 @@ def input_file(file):
     """Accept the file, check if it's the right one"""
 
     with open(file) as f:
-        p = re.compile(r"\S+.archimate")
-        if p.match(file) is not None:
-            # extension checks are passed
-            print("File extension is correct")
+        # check file extension and contents for correctness
+
+        ext_reg = re.compile(r"\S+.archimate")
+        if ext_reg.match(file) is not None:
+            # file extension check passed
+
+            cont_reg = re.compile(r"<archimate:model")
+            if cont_reg.findall(f.read()) == ["<archimate:model"]:
+                pass
+
+                # contents check passed
+            else:
+                print("File contents are incorrect.")
+
         else:
             print("Incorrect file extension")
