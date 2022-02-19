dc59589f422e140796ba61b8d0827f7686ab3d5f
Alex Sheyko <alex.sheyko@protonmail.com>
2022-02-19 22:55:03 +0200

    partial implementation of matched duplicates

--- src/menu_interface.py
+++ src/menu_interface.py
@@ -33,11 +33,45 @@ def main():
 
         def show_x_object():
             """Display all of the "X", where X is any archimate model object"""
-            query = input("Pass the object you wish to find(like DiagramObject): ")
+
+            query = input("Pass the object you wish to find(like DiagramObject): \n")
             pattern = root.xpath(f"//element[@* = 'archimate:{query}']")
             if not pattern:
                 pattern = root.xpath(f"//child[@* = 'archimate:{query}']")
             process_matches(pattern)
+
+        def show_matched_duplicates():
+            """Show all of the matched duplicates. A matched duplicate is one with
+            the same object type (business service for example), and identical or
+            slightly different (lowercase/uppercase/mixed case, or typo) names."""
+
+            query = input("Enter duplicates to find: ")
+            pattern = root.xpath(f"//element[@* = 'archimate:{query}']")
+
+            if not pattern:
+                pattern = root.xpath(f"//child[@* = 'archimate:{query}']")
+
+            result = ''
+            counter = 0
+            for child in pattern:
+                result += str(child.attrib)[45:] + '\n---------------------------------\n'
+                counter += 1
+
+            declare_duplicates = False
+            if counter > 1:
+                declare_duplicates = True
+
+            pu = PromptUtils(Screen())
+
+            if declare_duplicates == True:
+                pu.println(counter," duplicates have been found")
+            else:
+                print("No duplicates found")
+
+            pu.enter_to_continue()
+
+
+
 
         menu = ConsoleMenu(
             "Archibald", "A command line tool for inspecting archimate files."
@@ -50,10 +84,14 @@ def main():
             "Show Business Roles", display_business_roles
         )
         x_object = FunctionItem(
-            "Enter an object to find", show_x_object
+            "Search by name", show_x_object
+        )
+        matched_duplicates = FunctionItem(
+            "Show matched duplicates - to be implemented", show_matched_duplicates
         )
 
         menu.append_item(business_services)
         menu.append_item(business_roles)
         menu.append_item(x_object)
+        menu.append_item(matched_duplicates)
         menu.show()
