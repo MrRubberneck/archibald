70dcd1e15ea4b6badf1e132fca8e09dba2a7725d
Alex Sheyko <alex.sheyko@protonmail.com>
2022-02-28 21:02:33 +0200

    change 'yes' like statements to True/False, clean up overall, improve readability

--- src/archibald.py
+++ src/archibald.py
@@ -8,29 +8,26 @@ regex_for_checking_file_extension = re.compile(r"\S+.archimate")
 
 
 def is_correct_extension(file_name):
-    if regex_for_checking_file_extension.match(file_name) is not None:
-        return 'yes'
-    else:
-        print("Incorrect file extension")
-        raise SystemExit()
+    return regex_for_checking_file_extension.match(file_name) is not None
 
 
 def is_correct_contents(file_contents):
-    if "<archimate:model" in file_contents:
-        return 'yes'
-    else:
-        print("Incorrect file contents ")
-        raise SystemExit()
+    return "<archimate:mode is" in file_contents
 
 
 @click.command()
 @click.option("--file", help="Specify the archimate file")
 def process_file(file):
+    with open(file) as f:
+        file_contents = f.read()
+    if is_correct_extension(file):
+        if is_correct_contents(file_contents):
+            menu.menu()
 
-    with open(file) as file_contents:
-        if is_correct_extension(file) == 'yes':
-            if is_correct_contents(file_contents.read()) == 'yes':
-                menu.main()
+        else:
+            print("Invalid file contents")
+    else:
+        print("Invalid file extension")
 
 
 if __name__ == '__main__':
